0b5e2b535956c218b7f84b14c2b8726389dd2c69
Debian <bitnami@ip-172-26-13-205.ec2.internal>
2023-04-29 18:30:43 +0000

    working 2

---
+++ README.md
@@ -0,0 +1,37 @@
+## How to run 
+sudo flask run --host=0.0.0.0 --port=7001
+
+## structure 
+project/
+├── app.py
+├── templates/
+│   ├── index.html
+│   ├── chapters.html
+│   └── read.html
+└── static/
+    ├── css/
+    │   ├── style.css
+    │   └── reset.css
+    └── manga/
+        ├── manga1/
+        │   ├── chapter1/
+        │   │   ├── page1.jpg
+        │   │   ├── page2.jpg
+        │   │   └── ...
+        │   ├── chapter2/
+        │   │   ├── page1.jpg
+        │   │   ├── page2.jpg
+        │   │   └── ...
+        │   └── ...
+        ├── manga2/
+        │   ├── chapter1/
+        │   │   ├── page1.jpg
+        │   │   ├── page2.jpg
+        │   │   └── ...
+        │   ├── chapter2/
+        │   │   ├── page1.jpg
+        │   │   ├── page2.jpg
+        │   │   └── ...
+        │   └── ...
+        └── ...
+
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, abort, send_from_directory
+from flask import Flask, render_template, abort, send_from_directory, request
 import os
 
 app = Flask(__name__)
+app.static_folder = 'static'
 
 manga_folder = './static/manga'
 
@@ -24,7 +25,17 @@ def read(manga, chapter):
     if not os.path.exists(chapter_path):
         abort(404)
     pages = sorted(os.listdir(chapter_path))
-    return render_template('read.html', manga=manga, chapter=chapter, pages=pages)
+     # get the current page from the query parameters
+    page = request.args.get('page', pages[0])
+
+    # find the index of the current page in the list of pages
+    current_page_index = pages.index(page)
+
+    # set the previous and next pages
+    prev_page = pages[current_page_index - 1] if current_page_index > 0 else None
+    next_page = pages[current_page_index + 1] if current_page_index < len(pages) - 1 else None
+
+    return render_template('read.html', manga=manga, chapter=chapter, pages=pages,page=page, prev_page=prev_page, next_page=next_page)
 
 @app.route('/<manga>/<chapter>/<page>')
 def get_page(manga, chapter, page):
